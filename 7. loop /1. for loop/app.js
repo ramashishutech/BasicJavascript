67c8945bdbd347641863a8a059250d7151143315
ramashishutech <ramashishutech@gmail.com>
2023-09-21 22:08:37 +0530

    add loop file

---
+++ 7. loop /1. for loop/app.js
@@ -0,0 +1,21 @@
+
+/*
+for(initilization; condition; incriment/decriment){
+    // write you code here
+}
+*/
+
+
+for(let i=1; i<=100; i++){
+    console.log(i)
+}
+ 
+for(let a=1;a<=10;a++){
+    console.log(a*2);
+    
+
+    
+}
+
+
+
