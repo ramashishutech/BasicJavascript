5405f84571c6a6d0b52bd647014f0147ad68a842
ramashishutech <ramashishutech@gmail.com>
2023-09-23 21:15:01 +0530

    Addfile

---
+++ 10. data type conversion/app.js
@@ -0,0 +1,31 @@
+let type = 3;
+alert(typeof type);
+
+let b = true;
+alert(typeof b)
+
+
+//automatic conversion
+let c = 3 + null;
+console.log(c);
+alert(typeof c);
+
+let d = "5" * "yes"
+console.log(d)
+console.log('typeof :', typeof type);
+
+
+// conversion
+
+
+// 1. String()
+// 2. Number()
+// 3. Boolean()
+
+
+let e = 4;
+console.log('typeof e :', typeof e)
+
+let newType = Number(e)
+console.log('typeof newtype :', typeof newType)
+
