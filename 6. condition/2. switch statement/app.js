6dfa353167774e7eb99dba75e9a7150dadb6f3ba
ramashishutech <ramashishutech@gmail.com>
2023-09-26 22:26:14 +0530

    upadete in condition file and add new file

--- 6. condition/2. switch statement/app.js
+++ 6. condition/2. switch statement/app.js
@@ -42,7 +42,7 @@
 //}
 // my first Question how to find day by number
 // solve this quition  in belwo
-
+/*
 var day;
 day= prompt("plese enter the day number");
 switch(day){
@@ -69,7 +69,7 @@ switch(day){
         default:
         console.log("it is nat valid");
 
-}
+}*/
 
 //Quetion 2.how to find find manth name by number
 // var month;
@@ -128,3 +128,307 @@ switch(day){
 //             alert("it's number over the month number")
     
 // }
+var count =prompt("Enter the Number and obtended the number name");
+switch(count){
+    case "1":
+        alert("One");
+        break;
+    case'2':
+    alert("Two");
+    break;
+    case "3":
+        alert("three");
+        break;
+    case'4':
+    alert("Four");
+    break;
+    case "5":
+        alert("Five");
+        break;
+    case'6':
+    alert("six");
+    break;
+    case "7":
+        alert("Seven");
+        break;
+    case'8':
+    alert("Eight");
+    break;
+    case "9":
+        alert("Nine");
+        break;
+    case'10':
+    alert("Ten");
+    break;
+    case "11":
+        alert("Eleven");
+        break;
+    case'12':
+    alert("Twelve");
+    break;
+    case "13":
+        alert("Thirteen");
+        break;
+    case'14':
+    alert("Fourtheen");
+    break;
+    case "15":
+        alert("Fifteen");
+        break;
+    case'16':
+    alert("Sixteen");
+    break;
+    case "17":
+        alert("Seventeen");
+        break;
+    case'18':
+    alert("Eighteen");
+    break;
+    case "19":
+        alert("Ninteen");
+        break;
+    case'20':
+    alert("Twenty");
+    break;
+    case "21":
+        alert("TwentyOne");
+        break;
+    case'22':
+    alert("TwentyTwo");
+    break;
+    case "23":
+        alert("Twentythree");
+        break;
+    case'24':
+    alert("TwentyFour");
+    break;
+    case "25":
+        alert("TwentyFive");
+        break;
+    case'26':
+    alert("TwentySix");
+    break;
+    case "27":
+        alert("TwentySeven");
+        break;
+    case'28':
+    alert("TwentyEight");
+    break;
+    case "29":
+        alert("TwentyNine");
+        break;
+    case'30':
+    alert("Thirty");
+    break;
+    case "31":
+        alert("ThirtyOne");
+        break;
+    case'32':
+    alert("ThirtyTwo");
+    break;
+    case "34":
+        alert("ThirtyThree");
+        break;
+    case'35':
+    alert("Thirty_Five");
+    break;
+    case "36":
+        alert("Thirty_Six");
+        break;
+    case'37':
+    alert("Thirty_Seven");
+    break;
+    case "38":
+        alert("Thirty_Eight");
+        break;
+    case'39':
+    alert("Thirty_Nine");
+    break;
+    case "40":
+        alert("Forty");
+        break;
+    case'41':
+    alert("Forty_One");
+    break;
+    case "42":
+        alert("Forty_Two");
+        break;
+    case'43':
+    alert("Forty_Three");
+    break;
+    case "44":
+        alert("Forty_Four");
+        break;
+    case'45':
+    alert("Forty_Five");
+    break;
+    case "46":
+        alert("Forty_Six");
+        break;
+    case'47':
+    alert("Forty_Seven");
+    break;
+    case "48":
+        alert("Forty_Eight");
+        break;
+    case'49':
+    alert("Forty_Nine");
+    break;
+    case "50":
+        alert("Fifty");
+        break;
+    case'51':
+    alert("Fifty_One");
+    break;
+    case "52":
+        alert("Fifty_Two");
+        break;
+    case'53':
+    alert("Fifty_Three");
+    break;
+    case "54":
+        alert("Fifty_Four");
+        break;
+    case'55':
+    alert("Fifty_Five");
+    break;
+
+    case "56":
+        alert("Fifty_Six");
+        break;
+    case'57':
+    alert("Fifty_Seven");
+    break;
+    case "58":
+        alert("Fifty_Eight");
+        break;
+    case'59':
+    alert("Fifty_Nine");
+    break;
+    case "60":
+        alert("Sixty");
+        break;
+    case "61":
+        alert("Sixty_One");
+        break;
+    case "62":  
+        alert("Sixty_Two");
+        break;
+    case "63":
+        alert("Sixty_Three");
+        break;
+    case "64":
+        alert("Sixty_Four");
+        break;
+    case "65":
+        alert("Sixty_Five");
+        break;
+    case "66":
+        alert("Sixty_Six");
+        break;
+    case "67":
+        alert("Sixty_Seven");
+        break;
+    case "68":
+        alert("Sixty_Eight");
+        break;
+    case "69":
+        alert("Sixty_Nine");
+        break;
+    case "70":
+        alert("Seventy");
+        break;
+     case "71":
+        alert("Seventy_One");
+        break;
+    case "72":
+         alert("Seventy_Two");
+        break;
+    case "73":
+       alert("Seventy_Three");
+         break;
+    case "74":
+      alert("Seventy_Four");
+     break;
+    case "75":
+     alert("Seventy_five");
+         break;
+    case "76":
+      alert("Seventy_six");
+     break;
+    case "77":
+      alert("Seventy_Seven");
+     break;
+    case "78":
+      alert("Seventy_Eight");
+     break;
+     case "79":
+      alert("Seventy_Nine");
+     break;
+     case "80":
+        alert("Eighty");
+       break;
+    case "81":
+        alert("Eighty_one");
+       break;
+    case "82":
+        alert("Eighty_two");
+       break;
+    case "83":
+        alert("Eighty_three");
+       break;
+    case "84":
+        alert("Eighty_four");
+       break;
+    case "85":
+        alert("Eighty_five");
+       break;
+    case "86":
+        alert("Eighty_Six");
+       break;
+    case "87":
+        alert("Eighty_Seven");
+       break;
+    case "88":
+        alert("Eighty_eight");
+       break;  
+    case "89":
+        alert("Eighty_Nine");
+       break;
+    case "90":
+        alert("Ninty");
+       break;                                                                                                                                           
+    case "91":
+        alert("Ninty_one");
+       break;  
+    case "92":
+        alert("Ninty_two");
+       break;  
+    case "93":
+        alert("Ninty_three");
+       break;  
+    case "94":
+        alert("Ninty_four");
+       break;  
+    case "95":
+        alert("Ninty_five");
+       break;
+    case "96":
+        alert("Ninty_six");
+       break;   
+    case "97":
+        alert("Ninty_seven");
+       break; 
+    case "98":
+        alert("Ninty_eight");
+       break; 
+    case "99":
+        alert("Ninty_nine");
+       break; 
+       case "100":
+        alert("Hundred");
+       break; 
+    default:
+    alert("plese Enter the number under the 100")
+
+}
