5eea2c7dbe65a639a9357c322bfe189f642c7147
ramashishutech <ramashishutech@gmail.com>
2023-09-28 21:48:45 +0530

    update file

--- 11. string manipulation/app.js
+++ 11. string manipulation/app.js
@@ -67,8 +67,91 @@ let trimedText="   hellow     k                          "
 console.log(trimedText.trimStart());
 
 //uppercase 
-// this methode change the charecter the capital leter
-var a=`abcdefghijklmnopqrstuvwxyz`;
-console.log(a.toUpperCase);
+// this methode change the charecter the capital lete
+let lower = "abcdefghijklmnopqrstuvwxyz";
+let copital= lower.toUpperCase();
+console.log(lower);
 // lowerCase
 // this methode change the charecter the small leter
+let upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let small= upper.toLowerCase();
+console.log('it is lowercase',upper);
+//string lenght this method give the text lenght
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let l = text.length;
+console.log('it is lenght',l);
+// stringh slice methode
+//this methode do the slice between to prameter
+
+var a=`Ashish ,Annad,Ravi,Rahul`;
+var part=a.slice(7 ,13);
+console.log(`it is slice`,part)
+var a1=`Ashish ,Annad,Ravi,Rahul`;
+var part1=a1.slice(7);
+console.log(`it is slice onliy giveen one prameter`,part1);
+
+var a2=`Ashish ,Annad,Ravi,Rahul`;
+var part2=a2.slice(-12);
+console.log(`it is slice geven the single negative pramiter`,part2);
+
+var a3=`Ashish ,Annad,Ravi,Rahul`;
+var part3=a3.slice(-6-12);
+console.log(`it is slice methode given duble nagative pramiter`,part3);
+//string substring this mathode as like the slice methode
+let animal=`rat ,cat ,dog ,elephant`;
+let sub=animal.substring(5 ,8);
+console.log(`it is substring given to pramiter`,sub);
+
+// string replce methode this methode do the replece value
+var a=`my name is ashish kumar`;
+var rep=a.replace("ashish","Ramyash");
+console.log(`it is replace`,rep);
+//cancat methode- this methode join the two value
+var m=`ashish`
+var n=`kumar`;
+var d=m.concat(" " ,n);
+console.log(`it is concat methode`,d);
+//trim methode- this methode remove white sapce both side
+let t="             ashish       "
+let t1=t.trim();
+console.log(`it is trim`,t1);
+//trim start remove space starting text
+let charecter="             ashish"
+let t2=charecter.trimStart();
+console.log(`it is trimstrat`,t2);
+//trimend the remove the text end  ending time 
+let letter="ashish         "
+let letter1=t.trimEnd();
+console.log("it is trimend",letter1);
+
+//chart methode return the given posistion text value
+var c='hello word';
+var char= c.charAt(0);
+console.log(`it charAt`,char);
+//The charCodeAt() method returns the unicode of the character at a specified index in string
+var c='hello word';
+var char= c.charCodeAt(0);
+console.log(`charCodeAt`,char);
+
+//javaScript String split()
+//A  string can be convertd to an array with the split() methode
+let S="1,2,34,5,5";
+let sp=S.split(",");//split on commas
+console.log(`it is split`,sp);
+//(5) ['1', '2', '34', '5', '5']
+let S1="1, 2, 34, 5,5";
+let s=S1.split(" ");//split on space
+console.log(`it is split by space`,s);
+//(4) ['1,', '2,', '34,', '5,5']
+let S2="1, 2, 34, 5,5";
+let s1=S2.split(`it is split on pipe`," |");//split on pipe
+console.log(s1);
+// ['1, 2, 34, 5,5']
+//padStart methode this methd add the text befoet given string lenth
+var d="5";
+var n= d.padStart(9,"0");
+console.log(`it is padstart`,n);
+// output- 000000005
+
+//padEnd methode-this methode add the text ofter the given the string length
