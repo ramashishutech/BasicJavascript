7e91b6bf80298ea48d10537e0acf553468d4b57d
ramashishutech <ramashishutech@gmail.com>
2023-10-13 22:49:26 +0530

    add new file and do the some file update

--- 7. loop /1. for loop/app.js
+++ 7. loop /1. for loop/app.js
@@ -6,20 +6,26 @@ for(initilization; condition; incriment/decriment){
 */
 
 
-for(let i=1; i<=100; i++){
-    console.log(i)
-}
+// for(let i=1; i<=100; i++){
+//     console.log(i)
+// }
  
-for(let a=1;a<=10;a++){
-    console.log(a*2);
+// for(let a=1;a<=10;a++){
+//     console.log(a*2);
     
 
     
-}
+// }
 //print the revers counting 100 to 1;
-for(var n=100;n>0;n--){
-    console.log(n);
+// for(var n=100;n>0;n--){
+//     console.log(n);
+// }
+
+function multiply(number) {
+
+var number=parseInt(prompt("enter the number"));
+for (let index = 1; index <=10; index++) {
+    document.write(number+ "*"+index + " ="+ index*number +"<br>");
 }
-
-
-
+}
+multiply();
---
+++ All_quetion/19.return_the_AnArray/app.js
@@ -0,0 +1,18 @@
+//Q19 - return an arry with word inside a text
+
+var a="this is my house";
+var arr=[];
+for (let i = 0; i <a.length; i++) {
+         arr.push([i]);
+}console.log(arr);//it is first away but not exctly work
+
+//out put (17) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+var a='i am ashish';
+var ar=[];
+var c=a.split();
+for (let index = 0; index <a.length; index++){
+    console.log( typeof c[index]);
+    c[index].push(ar);
+};console.log(ar);
+
+// i can do the second away try bus  string
