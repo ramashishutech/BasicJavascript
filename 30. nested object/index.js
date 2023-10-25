c24777400d4a3c2823c48d06a3938a0483f3fbd0
ramashishutech <ramashishutech@gmail.com>
2023-10-25 23:02:51 +0530

    add new file and some update file

--- 30. nested object/index.js
+++ 30. nested object/index.js
@@ -21,3 +21,51 @@ console.log("obj",obj.address)
 console.log("obj",obj.address.familymembers.brother)
 console.log('SUM : ', obj.add(6,3));
 
+// this is nested object 
+
+let compny={
+    compnyName:"genius corporation",
+    compnyLcation:"up in ghazipur",
+    compnyEmpolye:50,
+    empolyedetels:{
+    Id:1,
+    Name:"Ashish",
+    Age:21,
+    Gender:"male",
+    Addres:{
+        village:"bhagirathpur",
+        post:"jalalabad",
+        dist:"ghazipur",
+        state:"uttar predesh",
+        contact:{
+            email:"ramashishutech@gmail.com",
+            mobileNo:7565852145,
+            telphon:7584
+        }
+    }
+}
+
+
+
+}
+console.log(compny);
+console.log(compny.empolyedetels);
+console.log(compny.empolyedetels.Id);
+console.log(compny.empolyedetels.Name);
+console.log(compny.empolyedetels.Age);
+console.log(compny.empolyedetels.Gender);
+console.log(compny.empolyedetels.Addres);
+console.log(compny.empolyedetels.Addres.village);
+console.log(compny.empolyedetels.Addres.post);
+console.log(compny.empolyedetels.Addres.dist);
+console.log(compny.empolyedetels.Addres.state);
+console.log(compny.empolyedetels.Addres.contact);
+console.log(compny.empolyedetels.Addres.contact.email)
+console.log(compny.empolyedetels.Addres.contact.mobileNo);
+console.log(compny.empolyedetels.Addres.contact.telphon);
+
+;
+
+
+
+
--- 31. hoisting/index.js
+++ 31. hoisting/index.js
@@ -11,3 +11,18 @@ a = 10;
 console.log(a)
 var a;   
 
+
+
+x=1;
+y=x;
+console.log('x='+ x)
+console.log('y='+y)
+var x;
+var y;
+
+//function hosting
+
+console.log(multiply(2,3));
+function multiply(a,b){
+    return a*b;
+}
