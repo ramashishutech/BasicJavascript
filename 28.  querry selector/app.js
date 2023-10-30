b29fde06f7982469bb28e519d16248a7f1dc334c
ramashishutech <ramashishutech@gmail.com>
2023-10-30 23:01:44 +0530

    Add new file

--- 28.  querry selector/app.js
+++ 28.  querry selector/app.js
@@ -2,8 +2,8 @@
 
 const querryS=document.querySelectorAll('.heading')
 const querry=document.querySelector('#heading')
-querryS[0].innerHTML="hello javascript"
+querryS[0].innerHTML=" i am from ghazipur"
 querryS[1].innerHTML="hello javascript"
 
-const q=document.querySelectorAll('.h3');
+const q=document.querySelectorAll('.h1');
 console.log(q);
